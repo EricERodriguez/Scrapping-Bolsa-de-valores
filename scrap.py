407020919f010e35d3ab5e7b629c51ce530d9b07
Eric R <eric92rodriguez@gmail.com>
2021-10-02 16:45:19 -0300

    iol

--- scrap.py
+++ scrap.py
@@ -8,13 +8,14 @@ from datetime import datetime
 
 
 # Definimos la url que queremos pedir
-url_page = 'https://www.ambito.com/contenidos/merval.html'
+url_page = 'https://iol.invertironline.com/mercado/cotizaciones'
 
 # tarda 480 milisegundos
 page = requests.get(url_page).text
 soup = BeautifulSoup(page, "lxml")
 
 # Hacemos la petición
-# tabla = soup.find('table', attrs={'id': 'table'})
-tabla = soup.find('tr', attrs={'td': ''})
-print(tabla)
+tabla = soup.find('table', attrs={'id': 'cotizaciones'})
+# tabla = soup.find('tr', attrs={'td': ''})
+# print(tabla.find_all('b'))
+print(tabla.select('b'))
